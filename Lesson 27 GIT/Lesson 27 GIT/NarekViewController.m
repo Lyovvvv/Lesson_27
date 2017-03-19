446978c6d36f4c7b82ba65f92680da0be3d4f646
Narek <narekab@gmail.com>
2017-03-19 13:29:51 +0400

    added NarekVC

---
+++ Lesson 27 GIT/Lesson 27 GIT/NarekViewController.m
@@ -0,0 +1,37 @@
+//
+//  NarekViewController.m
+//  Lesson 27 GIT
+//
+//  Created by NB Mac on 3/19/17.
+//  Copyright © 2017 Gharibyan Levon. All rights reserved.
+//
+
+#import "NarekViewController.h"
+
+@interface NarekViewController ()
+
+@end
+
+@implementation NarekViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
