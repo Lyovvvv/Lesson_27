5837cbedb5b4a60bf5b5ef502c9a15801fed1c82
Suren Poghosyan <poghosyan.suren.1998@gmail.com>
2017-03-19 13:54:24 +0400

    deleted methods

--- Lesson 27 GIT/Lesson 27 GIT/NarekViewController.m
+++ Lesson 27 GIT/Lesson 27 GIT/NarekViewController.m
@@ -18,12 +18,6 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
 }
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
-
 /*
 #pragma mark - Navigation
 
