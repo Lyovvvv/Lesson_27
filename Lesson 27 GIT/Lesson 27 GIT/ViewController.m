3c86a6e2204c32f99f73af1f04aee7fecd37ed5e
Narek <narekab@gmail.com>
2017-03-19 13:37:28 +0400

    added NSLog to ViewDidLoad

--- Lesson 27 GIT/Lesson 27 GIT/ViewController.m
+++ Lesson 27 GIT/Lesson 27 GIT/ViewController.m
@@ -19,6 +19,8 @@
     // Do any additional setup after loading the view, typically from a nib.
     // Hello
     NSLog(@"My Log");
+=======
+    NSLog(@"Test Conflict");
 }
 -(void)surensMethod{
     NSLog(@"Suren");
