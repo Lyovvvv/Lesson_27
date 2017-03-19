0cfd1a0e472acdf284f6fdd7ef749eb305a6d2df
Suren Poghosyan <poghosyan.suren.1998@gmail.com>
2017-03-19 13:37:21 +0400

    Surens Log

--- Lesson 27 GIT/Lesson 27 GIT/ViewController.m
+++ Lesson 27 GIT/Lesson 27 GIT/ViewController.m
@@ -18,6 +18,7 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     // Hello
+    NSLog(@"My Log");
 }
 -(void)surensMethod{
     NSLog(@"Suren");
