3acb5613df9aeb287726217779a312efe1d7462b
Sergey Benyaminyan <sbenyaminyan@gmail.com>
2017-03-19 13:27:25 +0400

    Sergey Class

---
+++ Lesson 27 GIT/Sergey_ ViewController.m
@@ -0,0 +1,37 @@
+//
+//  Sergey_ ViewController.m
+//  Lesson 27 GIT
+//
+//  Created by Mac on 3/19/17.
+//  Copyright © 2017 Gharibyan Levon. All rights reserved.
+//
+
+#import "Sergey_ ViewController.h"
+
+@interface Sergey__ViewController ()
+
+@end
+
+@implementation Sergey__ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
